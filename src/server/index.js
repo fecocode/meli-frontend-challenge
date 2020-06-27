31d0b6d182aeedca1960668f7d2cef2165bc98d5
Feco <hola@fecocode.com>
2020-06-27 12:57:30 -0300

    Inicializa proyecto
    Instala dependencias, inicializa el server para realizar SSR con él.

---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import compression from "compression";
+import index from "./routes/index";
+import path from "path";
+
+// Instacia servidor
+const app = express();
+
+// Configura el motor de vistas SSR
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+
+// Middleware
+app.use(compression());
+console.log(__dirname);
+app.use(express.static(__dirname + "/public"));
+
+// Rutas
+app.use("/", index);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function listenHandler() {
+    console.info(`Running on ${port}`)
+});
